94efe9478b71df8638031473a42c0c0840550091
Kazuhiro Sera <seratch@gmail.com>
2015-11-16 14:46:35 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -2,26 +2,25 @@
 
 sbt_version=$1
 if [ "${sbt_version}" == "" ];then
-  echo "Specify sbt version (e.g. 0.13.9)"
-  exit 1
+  sbt_version="0.13.9"
 fi
 
 repo_base_url="https://github.com/seratch/sbt-quick-boot"
 filename="ivy2_cache_${sbt_version}.zip"
-wget "${repo_base_url}/raw/master/${filename}"
+curl -L "${repo_base_url}/raw/master/${filename}" -o ${filename}
 unzip ${filename}
 rm -f ${filename}
 
-mkdir -p $HOME/.ivy/cache
-mv ${sbt_version}/cache/* $HOME/.ivy2/cache/.
-rm -rf ${sbt_version}/cache
+mkdir -p $HOME/.ivy2/cache
+cp -pr ${sbt_version}/cache/* $HOME/.ivy2/cache/.
+rm -rf ${sbt_version}
 
-wget "${repo_base_url}/raw/master/sbt"
+curl -L "${repo_base_url}/raw/master/sbt" -o sbt
 chmod +x sbt
 
-mkdir bin
+mkdir -p bin
 cd bin
-wget "${repo_base_url}/raw/master/bin/sbt-launch.jar"
+curl -L "${repo_base_url}/raw/master/bin/sbt-launch.jar" -o sbt-launch.jar
 
 cd -
 ./sbt
