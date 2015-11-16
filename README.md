cd618255937503489e89015d82f4ab853274f396
Kazuhiro Sera <seratch@gmail.com>
2015-11-16 14:54:17 +0900

    Remove zip file and update install.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sbt-quick-boot
-sbt quick boot script
+
+A setup script for my personal use.
+
+```
+curl -L https://github.com/seratch/sbt-quick-boot/raw/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if [ "${sbt_version}" == "" ];then
   sbt_version="0.13.9"
 fi
 
-repo_base_url="https://github.com/seratch/sbt-quick-boot"
+repo_base_url="https://github.com/seratch/sbt-quick-boot/releases/download/${sbt_version}/"
 filename="ivy2_cache_${sbt_version}.zip"
-curl -L "${repo_base_url}/raw/master/${filename}" -o ${filename}
+curl -L "${repo_base_url}/${filename}" -o ${filename}
 unzip ${filename}
 rm -f ${filename}
 
@@ -15,12 +15,12 @@ mkdir -p $HOME/.ivy2/cache
 cp -pr ${sbt_version}/cache/* $HOME/.ivy2/cache/.
 rm -rf ${sbt_version}
 
-curl -L "${repo_base_url}/raw/master/sbt" -o sbt
+curl -L "${repo_base_url}/sbt" -o sbt
 chmod +x sbt
 
 mkdir -p bin
 cd bin
-curl -L "${repo_base_url}/raw/master/bin/sbt-launch.jar" -o sbt-launch.jar
+curl -L "${repo_base_url}/sbt-launch.jar" -o sbt-launch.jar
 
 cd -
 ./sbt
